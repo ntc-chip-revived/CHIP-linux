eb940c5f9b5404b987895bf566ff48fdd437fb96
Richard Weinberger <richard@nod.at>
2016-04-17 17:32:45 +0200

    UBI: Fixup ubi_io_read_vid_hdrs() wrt. SLC mode
    Signed-off-by: Richard Weinberger <richard@nod.at>

--- drivers/mtd/ubi/io.c
+++ drivers/mtd/ubi/io.c
@@ -1177,9 +1177,6 @@ int ubi_io_read_vid_hdrs(struct ubi_device *ubi, int pnum,
 
 		magic = be32_to_cpu(vid_hdr->magic);
 		if (magic != UBI_VID_HDR_MAGIC) {
-			if (i && !magic)
-				break;
-
 			if (mtd_is_eccerr(read_err)) {
 				err = UBI_IO_BAD_HDR_EBADMSG;
 				break;
@@ -1195,6 +1192,14 @@ int ubi_io_read_vid_hdrs(struct ubi_device *ubi, int pnum,
 					err = UBI_IO_FF;
 				else
 					err = UBI_IO_FF_BITFLIPS;
+
+				/*
+				 * Propagate UBI_IO_FF/_BITFLIPS only for the first VID header,
+				 * otherwise UBI will treat the whole PEB as empty.
+				 */
+				if (i)
+					err = 0;
+
 				break;
 			}
 
